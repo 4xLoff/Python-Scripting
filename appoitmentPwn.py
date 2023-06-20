994f17afa3c1c096246204c5bfc94f2bdcb2995a
4xL <77083140+4xLoff@users.noreply.github.com>
2023-06-20 15:44:21 -0500

    Update appoitmentPwn.py

--- appoitmentPwn.py
+++ appoitmentPwn.py
@@ -3,6 +3,7 @@
 import argparse
 import requests
 import re
+from colorama import Fore, Style
 
 class Exploit:
     def __init__(self, ip_address):
@@ -22,11 +23,11 @@ class Exploit:
         flag = flag.strip()  # Eliminar espacios en blanco alrededor de la bandera
 
         print
-        print(f"\nEl contenido de la 'flag': ", flag)
+        print(Fore.GREEN + f"\nEl contenido de la 'flag': {flag}\n" + Style.RESET_ALL)
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Uso de AutoPwn')
-    parser.add_argument('-i', '--ip', dest='ip_address', required=True, help='IP de host remoto')
+    parser.add_argument('-i', '--ip',default=80, dest='ip_address', required=False, help='IP de host remoto')
     return parser.parse_args()
 
 def main():
